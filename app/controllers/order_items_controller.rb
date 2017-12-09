25b018fc068b43d6a9b2462f3c5975e79ef7cc56
safiy cham <csafi13@yahoo.com>
2017-12-08 20:49:50 -0800

    add AJAX to toggle show-hide of each product on index page.

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -6,11 +6,8 @@ class OrderItemsController < ApplicationController
     if @order.save
       session[:order_id] = @order.id
       respond_to do |format|
-        format.html { redirect_to products_path }
         format.js
       end
-    else
-      redirect_to products_path
     end
   end
 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,10 +4,15 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     @order_item = current_order.order_items.new
+
   end
 
   def new
     @product = Product.new
+  end
+
+  def show
+    @product = Product.find(params[:id])
   end
 
   def create
