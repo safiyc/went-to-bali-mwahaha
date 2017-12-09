0db56bf7c4dc21ab0589f9d4d59f1a191386beca
safiy cham <csafi13@yahoo.com>
2017-12-09 15:59:40 -0800

    add disable user from inputting a negative qty of items.

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,6 +2,6 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
-  validates :quantity, presence: true
+  validates :quantity, numericality: { greater_than_or_equal_to: 1 }, presence: true
 
 end
