d31beae2ae1c64f0e62dc87a85b1dc27dac3a20f
safiy cham <csafi13@yahoo.com>
2017-12-09 17:38:38 -0800

    add action for admin to delete product.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
   before_filter :authorize, except: [:index, :show]
 
   def index
@@ -7,7 +8,7 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
+    # @product = Product.find(params[:id])
   end
 
   def new
@@ -24,11 +25,11 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
+    # @product = Product.find(params[:id])
   end
 
   def update
-    @product = Product.find(params[:id])
+    # @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to product_path(@product)
     else
@@ -36,8 +37,17 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product.destroy
+    redirect_to root_path
+  end
+
   private
   def product_params
     params.require(:product).permit(:name, :description, :price)
+  end
+
+  def find_product
+    @product = Product.find(params[:id])
   end
 end
